0e0989653336e9507fc1bb3661c0d5455ef893d9
Pedro Almeida <pdhmarques@gmail.com>
2017-10-29 21:38:11 -0200

    Add Arvore

--- src/ArvoreAVL.java
+++ src/ArvoreAVL.java
@@ -1,10 +1,93 @@
 public class ArvoreAVL {
 
-    //Badaro
+    private int chave;
+    private ArvoreAVL esq;
+    private ArvoreAVL dir;
 
+    public ArvoreAVL(int chave, ArvoreAVL esq, ArvoreAVL dir){
+        setChave(chave);
+        setDir(dir);
+        setEsq(esq);
+    }
 
-    //Gabriel
+    public ArvoreAVL(){
+        this(-1,null,null);
+    }
 
+    public ArvoreAVL(int chave){
+        this(chave,new ArvoreAVL(), new ArvoreAVL());
+    }
 
-    //Pedro
+    public ArvoreAVL getDir() {
+        return dir;
+    }
+
+    public ArvoreAVL getEsq() {
+        return esq;
+    }
+
+    public int getChave() {
+        return chave;
+    }
+
+    public void setChave(int chave) {
+        this.chave = chave;
+    }
+
+    public void setDir(ArvoreAVL dir) {
+        this.dir = dir;
+    }
+
+    public void setEsq(ArvoreAVL esq) {
+        this.esq = esq;
+    }
+
+    public boolean estahVazia(){
+        boolean vazia = false;
+        if(getEsq() == null && getDir() == null){
+            vazia = true;
+        }
+        return vazia;
+    }
+
+    public void anexa(int chave){
+        setChave(chave);
+        setEsq(new ArvoreAVL());
+        setDir(new ArvoreAVL());
+    }
+
+    public boolean insere(int chave){
+        boolean inserido = true;
+
+        if(estahVazia())anexa(chave);
+        else{
+            if(chave<getChave())esq.insere(chave);
+            else if(chave>this.getChave())dir.insere(chave);
+            else inserido = false;
+        }
+
+        return inserido;
+    }
+
+    public void preOrdem(){
+        if(estahVazia())return;
+        System.out.println(this.getChave() + ", ");
+        esq.preOrdem();
+        dir.preOrdem();
+    }
+
+    public void posOrdem(){
+
+        if(estahVazia())return;
+        esq.posOrdem();
+        dir.posOrdem();
+        System.out.println(this.getChave() + ", ");
+    }
+
+    public void emOrdem(){
+        if(estahVazia())return;
+        esq.emOrdem();
+        System.out.println(this.getChave()+ ", ");
+        dir.emOrdem();
+    }
 }

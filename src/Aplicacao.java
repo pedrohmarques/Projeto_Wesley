ca756251b90476d1e4764b758febc37ce8dac7fd
gmfonseca <32571975+gmfonseca@users.noreply.github.com>
2017-11-06 20:49:43 -0200

    Adicionado método 'gravou()' à 'main()'

--- src/Aplicacao.java
+++ src/Aplicacao.java
@@ -5,13 +5,15 @@ public class Aplicacao {
         arvoreAVL.insere(5);
         arvoreAVL.insere(6);
         arvoreAVL.insere(1);
-        arvoreAVL.insere(8);
-        arvoreAVL.insere(10);
         arvoreAVL.insere(4);
         arvoreAVL.insere(7);
 
-        arvoreAVL.Remocao(arvoreAVL, 7);
+        System.out.println(arvoreAVL.emLargura());
 
-        arvoreAVL.emLargura();
+        if(arvoreAVL.gravou()){
+            System.out.println("GRAVADO COM SUCESSO");
+        }else{
+            System.out.println("ERROR: Não foi possível gravar a árvore.");
+        }
     }
 }
